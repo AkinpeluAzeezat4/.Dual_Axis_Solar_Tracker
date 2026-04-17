bfa5ab97f0ae9e1ee3489eae5bbaa949eec73f4b
Egbe Raymond <jsitech4@gmail.com>
2026-04-17 11:27:29 +0100

    Change in ledPin lib
    Enhance LED indicator functionality with PWM and pause logic.

--- ESP32/Fingerprint_and_RFID_Card_Attendance_System/code/lib/Display/led_indicator/led_indicator.cpp
+++ ESP32/Fingerprint_and_RFID_Card_Attendance_System/code/lib/Display/led_indicator/led_indicator.cpp
@@ -1,12 +1,84 @@
 #include <Arduino.h>
 #include "led_indicator.h"
 
-namespace led_indicator {
+namespace led_indicator
+{
+  int ledPin;
+  const int pwmChannel = 0;
+  const int pwmFreq = 10000;
+  const int pwmResolution = 8;
 
-void begin() {
-}
+  int brightness = 0;
+  bool rising = true;
 
-void update() {
-}
+  unsigned long lastUpdate = 0;
+  const int stepTime = 1;
 
+  int pulseCount = 0;
+
+  bool inPause = false;
+  unsigned long pauseStart = 0;
+  const int pauseDuration = 1000;
+
+  void begin(uint8_t pin)
+  {
+    ledPin = pin;
+
+    ledcSetup(pwmChannel, pwmFreq, pwmResolution);
+    ledcAttachPin(ledPin, pwmChannel);
+  }
+
+  void update()
+  {
+    unsigned long now = millis();
+
+    // handle pause state (non-blocking)
+    if (inPause)
+    {
+      if (now - pauseStart >= pauseDuration)
+      {
+        inPause = false;
+      }
+      else
+      {
+        return;
+      }
+    }
+
+    if (now - lastUpdate < stepTime)
+      return;
+
+    lastUpdate = now;
+
+    if (rising)
+    {
+      brightness += 1;
+
+      if (brightness >= 255)
+      {
+        brightness = 255;
+        rising = false;
+      }
+    }
+    else
+    {
+      brightness -= 1;
+
+      if (brightness <= 0)
+      {
+        brightness = 0;
+        rising = true;
+        pulseCount++;
+
+        if (pulseCount >= 2)
+        {
+          pulseCount = 0;
+          inPause = true;
+          pauseStart = now;
+        }
+      }
+    }
+
+    ledcWrite(pwmChannel, brightness);
+  }
 }
