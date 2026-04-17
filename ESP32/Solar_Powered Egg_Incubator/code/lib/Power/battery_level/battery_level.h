146e04f01fb068115c16e772eb15b464eb822058
Egbe Raymond <jsitech4@gmail.com>
2026-04-17 11:38:21 +0100

    Change in battery level lib
    Refactor battery level functions to include pin and scale parameters.

--- ESP32/Solar_Powered Egg_Incubator/code/lib/Power/battery_level/battery_level.h
+++ ESP32/Solar_Powered Egg_Incubator/code/lib/Power/battery_level/battery_level.h
@@ -1,13 +1,14 @@
-#pragma once
+#ifndef BATTERY_LEVEL_H
+#define BATTERY_LEVEL_H
+
 #include <Arduino.h>
 
 namespace battery_level
 {
-  void begin();
+  void begin(uint8_t pin, float scale = 1.61f);
   void update();
 
-  float voltage();
-  uint8_t percentage();
-
-  bool isLow();
+  float getVoltage();
 }
+
+#endif
